9db90ffafaf56d51852a822aaea8ffa6a55d4282
Andrey <KonkeyDong37@gmail.com>
2020-11-30 13:56:37 +0500

    Initial Commit

---
+++ SwiftUIDemo/SwiftUIDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIDemoApp.swift
+//  SwiftUIDemo
+//
+//  Created by Андрей on 30.11.2020.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
